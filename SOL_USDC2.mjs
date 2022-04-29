b348dcbd7dff550d011d801a77fff6fa4d22cbec
durianpool <81219935+durianpool@users.noreply.github.com>
2022-04-29 15:47:21 +0800

    Create SOL_USDC2.mjs

---
+++ SOL_USDC2.mjs
@@ -0,0 +1,226 @@
+import dotenv from "dotenv";
+import bs58 from "bs58";
+import {
+  Connection,
+  Keypair,
+  Transaction,
+  PublicKey,
+  SystemProgram,
+} from "@solana/web3.js";
+import got from "got";
+import { Wallet } from "@project-serum/anchor";
+import promiseRetry from "promise-retry";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  Token,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+
+console.log({ dotenv });
+dotenv.config();
+
+// This is a free Solana RPC endpoint. It may have ratelimit and sometimes
+// invalid cache. I will recommend using a paid RPC endpoint.
+const connection = new Connection("https://ssc-dao.genesysgo.net/");
+const wallet = new Wallet(
+  Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY || ""))
+);
+
+const USDC_MINT = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
+const SOL_MINT = "So11111111111111111111111111111111111111112";
+
+const PROFIT_BPS = 1.0023;
+
+// wsol account
+const createWSolAccount = async () => {
+  const wsolAddress = await Token.getAssociatedTokenAddress(
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+    TOKEN_PROGRAM_ID,
+    new PublicKey(SOL_MINT),
+    wallet.publicKey
+  );
+
+  const wsolAccount = await connection.getAccountInfo(wsolAddress);
+
+  if (!wsolAccount) {
+    const transaction = new Transaction({
+      feePayer: wallet.publicKey,
+    });
+    const instructions = [];
+
+    instructions.push(
+      await Token.createAssociatedTokenAccountInstruction(
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        new PublicKey(SOL_MINT),
+        wsolAddress,
+        wallet.publicKey,
+        wallet.publicKey
+      )
+    );
+
+    // fund 1 sol to the account
+    instructions.push(
+      SystemProgram.transfer({
+        fromPubkey: wallet.publicKey,
+        toPubkey: wsolAddress,
+        lamports: 1_000_000_000, // 1 sol
+      })
+    );
+
+    instructions.push(
+      // This is not exposed by the types, but indeed it exists
+      Token.createSyncNativeInstruction(TOKEN_PROGRAM_ID, wsolAddress)
+    );
+
+    transaction.add(...instructions);
+    transaction.recentBlockhash = await (
+      await connection.getRecentBlockhash()
+    ).blockhash;
+    transaction.partialSign(wallet.payer);
+    const result = await connection.sendTransaction(transaction, [
+      wallet.payer,
+    ]);
+    console.log({ result });
+  }
+
+  return wsolAccount;
+};
+
+const getCoinQuote = (inputMint, outputMint, amount) =>
+  got
+    .get(
+      `https://quote-api.jup.ag/v1/quote?outputMint=${outputMint}&inputMint=${inputMint}&amount=${amount}&slippage=0.2`
+    )
+    .json();
+
+const getTransaction = (route) => {
+  return got
+    .post("https://quote-api.jup.ag/v1/swap", {
+      json: {
+        route: route,
+        userPublicKey: wallet.publicKey.toString(),
+        // to make sure it doesnt close the sol account
+        wrapUnwrapSOL: false,
+      },
+    })
+    .json();
+};
+
+const getConfirmTransaction = async (txid) => {
+  const res = await promiseRetry(
+    async (retry, attempt) => {
+      let txResult = await connection.getTransaction(txid, {
+        commitment: "confirmed",
+      });
+
+      if (!txResult) {
+        const error = new Error("Transaction was not confirmed");
+        error.txid = txid;
+
+        retry(error);
+        return;
+      }
+      return txResult;
+    },
+    {
+      retries: 40,
+      minTimeout: 500,
+      maxTimeout: 1000,
+    }
+  );
+  if (res.meta.err) {
+    throw new Error("Transaction failed");
+  }
+  return txid;
+};
+
+// require wsol to start trading, this function create your wsol account and fund 1 SOL to it
+await createWSolAccount();
+
+// initial 100 USDC for quote
+const initial = 100_000_000;
+
+// initial 1 SOL for quote
+const initial_token = 1_000_000_000;
+
+while (true) {
+  // 0.1 SOL
+  const usdcToSol = await getCoinQuote(USDC_MINT, SOL_MINT, initial);
+  const solToUsdc = await getCoinQuote(SOL_MINT, USDC_MINT, usdcToSol.data[0].outAmount);
+  
+  // when outAmount more than initial
+  if (solToUsdc.data[0].outAmount > initial*PROFIT_BPS) {
+    await Promise.all(
+      [usdcToSol.data[0], solToUsdc.data[0]].map(async (route) => {
+        const { setupTransaction, swapTransaction, cleanupTransaction } =
+          await getTransaction(route);
+
+        await Promise.all(
+          [setupTransaction, swapTransaction, cleanupTransaction]
+            .filter(Boolean)
+            .map(async (serializedTransaction) => {
+              // get transaction object from serialized transaction
+              const transaction = Transaction.from(
+                Buffer.from(serializedTransaction, "base64")
+              );
+              // perform the swap
+              // Transaction might failed or dropped
+              const txid = await connection.sendTransaction(
+                transaction,
+                [wallet.payer],
+                {
+                  skipPreflight: true,
+                }
+              );
+              try {
+                await getConfirmTransaction(txid);
+                console.log(`USDC_Success: https://solscan.io/tx/${txid}`);
+              } catch (e) {
+                console.log(`USDC_Failed: https://solscan.io/tx/${txid}`);
+              }
+            })
+        );
+      })
+    );
+  };
+  
+  const solToUsdc_r = await getCoinQuote(SOL_MINT, USDC_MINT, initial_token);
+  const usdcToSol_r = await getCoinQuote(USDC_MINT, SOL_MINT, solToUsdc_r.data[0].outAmount);
+
+  // when outAmount more than initial
+  if (usdcToSol_r.data[0].outAmount > initial_token*PROFIT_BPS) {
+    await Promise.all(
+      [solToUsdc_r.data[0], usdcToSol_r.data[0]].map(async (route) => {
+        const { setupTransaction, swapTransaction, cleanupTransaction } =
+          await getTransaction(route);
+
+        await Promise.all(
+          [setupTransaction, swapTransaction, cleanupTransaction]
+            .filter(Boolean)
+            .map(async (serializedTransaction) => {
+              // get transaction object from serialized transaction
+              const transaction = Transaction.from(
+                Buffer.from(serializedTransaction, "base64")
+              );
+              // perform the swap
+              // Transaction might failed or dropped
+              const txid = await connection.sendTransaction(
+                transaction,
+                [wallet.payer],
+                {
+                  skipPreflight: true,
+                }
+              );
+              try {
+                await getConfirmTransaction(txid);
+                console.log(`TOKEN_Success: https://solscan.io/tx/${txid}`);
+              } catch (e) {
+                console.log(`TOKEN_Failed: https://solscan.io/tx/${txid}`);
+              }
+            })
+        );
+      })
+    );
+  }
+}
